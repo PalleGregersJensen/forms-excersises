fec22c7613617f23815803d947139dcb53cfb1b0
PalleGregersJensen <palle.gregers.jensen@gmail.com>
2023-04-20 21:18:30 +0200

    App for seven year rule works

---
+++ seven_year_rule.js
@@ -0,0 +1,31 @@
+"use strict"
+
+console.log("Js kører");
+
+window.addEventListener("load", initApp);
+
+function initApp() {
+    document.querySelector("#submit_btn").addEventListener("click", myAgeDividedByTwoAndAddedSeven);    
+}
+
+function myAgeDividedByTwoAndAddedSeven() {
+console.log("myAge-function")
+
+    let myAge = document.querySelector("#myAge").value;
+    let ageYoungWoman = document.querySelector("#age_young_woman").value;
+
+    // ----Turn myAge to numnber----
+    // myAge * 1;
+
+    let tooYoung = (myAge/2) + 7;
+    
+    if (tooYoung > ageYoungWoman) {
+        document.querySelector("#output").textContent = "Too young";
+    }
+    else {
+        document.querySelector("#output").textContent = "It is ok";
+    }
+    console.log(tooYoung);
+    console.log(ageYoungWoman);
+
+}
